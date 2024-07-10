cfaf8b638a2c94cf4733c8ae2d6e792a96f2ba69
AndreaSdr70 <andrea@sdringola.it>
2024-07-10 23:25:23 +0200

    Selfwork Variabili

---
+++ script.js
@@ -0,0 +1,23 @@
+let num1 = 5;
+
+let num2 = 10;
+
+console.log(num1);
+
+console.log(num2);
+
+let str = "pietra";
+
+console.log(str);
+
+str = "pluto";
+
+console.log(str);
+
+const PIGRECO = 3.14159265359;
+
+console.log(PIGRECO);
+
+const PIGRECO = 2;
+
+
